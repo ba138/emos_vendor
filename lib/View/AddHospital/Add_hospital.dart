e94dab4e58f1578f0d817a1ad3907e0f64690f9c
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-02-21 19:22:38 +0500

    working on hospital home

--- lib/View/AddHospital/Add_hospital.dart
+++ lib/View/AddHospital/Add_hospital.dart
@@ -1,6 +1,7 @@
 import 'package:emos_vendor/components/RoundedButton/rounded_button.dart';
 import 'package:emos_vendor/components/VerticalSpacing/vertical_spacing.dart';
 import 'package:emos_vendor/components/appBarField/appBar_field.dart';
+import 'package:emos_vendor/routes/routes_name.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -210,7 +211,9 @@ class AddHospital extends StatelessWidget {
                 const VerticalSpeacing(43.0),
                 RoundedButton(
                   title: 'Continue',
-                  onpress: () {},
+                  onpress: () {
+                    Navigator.pushNamed(context, RouteName.hospitalHome);
+                  },
                   bgColor: AppColor.bgFillColor,
                   titleColor: AppColor.whiteColor,
                 ),
