b9a0f05c5e1369afce6c49f523063559be3a3905
Basit ali <basitalyshah51214@gmail.com>
2024-03-05 02:28:05 -0800

    changes to add payment detail screen

---
+++ lib/View/Hospital/profile/payment_detail_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PaymentDetailView extends StatelessWidget {
+  const PaymentDetailView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
