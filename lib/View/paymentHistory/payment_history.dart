7dbe33ad3147a1b4e0a32db15058b4c20d6c2c9e
Hasnain Haider <hasnainhaiderbs2099@gmail.com>
2024-03-05 20:56:16 +0500

    working on payemnt screen

--- lib/View/paymentHistory/payment_history.dart
+++ lib/View/paymentHistory/payment_history.dart
@@ -3,59 +3,100 @@ import 'package:flutter/material.dart';
 
 class PymentHistory extends StatelessWidget {
   const PymentHistory({super.key});
-
+  final double tHeight = 374.0;
+  final double top = 301.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            height: 200,
-            width: double.infinity,
-            color: AppColor.bgFillColor,
-            child: Stack(
-              children: [
-                Positioned(
-                  bottom: 0,
-                  left: 0,
-                  right: 0,
-                  child: Row(
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                  height: 200,
+                  width: double.infinity,
+                  color: AppColor.bgFillColor,
+                  child: Column(
                     children: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(
-                          Icons.arrow_back_ios_new,
-                          color: AppColor.whiteColor,
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: const Icon(
+                                Icons.arrow_back_ios_new,
+                                color: AppColor.whiteColor,
+                              ),
+                            ),
+                            SizedBox(
+                                width: MediaQuery.of(context).size.width / 5),
+                            const Text(
+                              'Payment History',
+                              style: TextStyle(
+                                color: AppColor.hometxtColor,
+                                fontSize: 24.0,
+                              ),
+                            )
+                          ],
                         ),
                       ),
-                      SizedBox(width: MediaQuery.of(context).size.width / 5),
-                      const Text(
-                        'Payment History',
-                        style: TextStyle(
-                          color: AppColor.hometxtColor,
-                          fontSize: 24.0,
+                      Positioned(
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        child: Row(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              icon: const Icon(
+                                Icons.arrow_back_ios_new,
+                                color: AppColor.whiteColor,
+                              ),
+                            ),
+                            SizedBox(
+                                width: MediaQuery.of(context).size.width / 5),
+                            const Text(
+                              'Payment History',
+                              style: TextStyle(
+                                color: AppColor.hometxtColor,
+                                fontSize: 24.0,
+                              ),
+                            )
+                          ],
                         ),
-                      )
+                      ),
                     ],
-                  ),
-                ),
-                Positioned(
-                  bottom: -50,
-                  left: 20.0,
-                  right: 20.0,
-                  child: Container(
-                    height: 100,
-                    width: 200,
-                    color: Colors.red,
-                  ),
-                ),
-              ],
-            ),
+                  )),
+            ],
           ),
         ],
       ),
     );
   }
 }
+
+// _buildCoverBar() {
+//   return Container(
+//     height: tHeight,
+//     width: double.infinity,
+//     decoration: const BoxDecoration(
+//       gradient: LinearGradient(
+//         begin: Alignment.topCenter,
+//         end: Alignment.bottomCenter,
+//         colors: [Color(0xff2AA39C), Color(0xff2AA39C)],
+//       ),
+//       borderRadius: BorderRadius.only(
+//         bottomLeft: Radius.circular(30.0),
+//         bottomRight: Radius.circular(30.0),
+//       ),
+//     ),
+//   );
+// }
