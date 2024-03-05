80874ab7d6da03401804d0fdde3c026146f88883
Basit ali <basitalyshah51214@gmail.com>
2024-03-05 09:46:05 -0800

    adding payment detail screen

--- lib/View/payment_detail_view.dart
+++ lib/View/payment_detail_view.dart
@@ -1,10 +1,117 @@
+import 'package:emos_vendor/GlobalColors/colors.dart';
+import 'package:emos_vendor/components/VerticalSpacing/vertical_spacing.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PaymentDetailView extends StatelessWidget {
   const PaymentDetailView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColor.bgFillColor,
+      appBar: AppBar(
+        toolbarHeight: 80,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios_new,
+              color: AppColor.whiteColor,
+            )),
+        title: const Text(
+          'Payment Details',
+          style: TextStyle(color: AppColor.whiteColor),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          color: AppColor.whiteColor,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(50.0),
+            topLeft: Radius.circular(50.0),
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Container(
+                    height: 138,
+                    width: 138,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      image: const DecorationImage(
+                        image: NetworkImage(
+                            "https://media.istockphoto.com/id/1311511363/photo/headshot-portrait-of-smiling-male-doctor-with-tablet.jpg?s=612x612&w=0&k=20&c=w5TecWtlA_ZHRpfGh20II-nq5AvnhpFu6BfOfMHuLMA="),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ),
+                const VerticalSpeacing(12),
+                Center(
+                  child: Text(
+                    "Basit Aly",
+                    style: GoogleFonts.getFont(
+                      "Roboto",
+                      textStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                        color: AppColor.textColor,
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "patient",
+                        style: GoogleFonts.getFont(
+                          "Roboto",
+                          textStyle: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w300,
+                            color: AppColor.bgFillColor,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Container(
+                        height: 21,
+                        width: 21,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              2,
+                            ),
+                            color: AppColor.bgFillColor),
+                        child: const Icon(
+                          Icons.call_outlined,
+                          size: 14,
+                          color: AppColor.whiteColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
