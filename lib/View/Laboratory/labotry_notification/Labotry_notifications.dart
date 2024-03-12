fdcda249d25069869f99505d06f62902cf05fe90
Basit ali <basitalyshah51214@gmail.com>
2024-03-12 10:55:46 -0700

    changes to fix the issues of the app

--- lib/View/Laboratory/labotry_notification/Labotry_notifications.dart
+++ lib/View/Laboratory/labotry_notification/Labotry_notifications.dart
@@ -1,10 +1,10 @@
 // ignore_for_file: file_names
 
+import 'package:emos_vendor/View/DoctorAtHome/widget/patient_detail_dialog.dart';
+import 'package:emos_vendor/View/Hospital/notifications/widgets/notificationWidget.dart';
 import 'package:flutter/material.dart';
 
 import '../../../GlobalColors/colors.dart';
-import '../../../components/VerticalSpacing/vertical_spacing.dart';
-import 'widgets/Labotry_notificationWidget.dart';
 
 class LabotryNotificationScreen extends StatelessWidget {
   const LabotryNotificationScreen({super.key});
@@ -42,53 +42,20 @@ class LabotryNotificationScreen extends StatelessWidget {
         ),
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: ListView(
-            children: const [
-              Column(
-                children: [
-                  VerticalSpeacing(20.0),
-                  LabotryNotificationsWidget(),
-                  VerticalSpeacing(10.0),
-                  Divider(color: AppColor.blackColor),
-                  VerticalSpeacing(20.0),
-                  LabotryNotificationsWidget(),
-                  VerticalSpeacing(10.0),
-                  Divider(color: AppColor.blackColor),
-                  VerticalSpeacing(20.0),
-                  LabotryNotificationsWidget(),
-                  VerticalSpeacing(10.0),
-                  Divider(color: AppColor.blackColor),
-                  VerticalSpeacing(20.0),
-                  LabotryNotificationsWidget(),
-                  VerticalSpeacing(10.0),
-                  Divider(color: AppColor.blackColor),
-                  VerticalSpeacing(20.0),
-                  LabotryNotificationsWidget(),
-                  VerticalSpeacing(10.0),
-                  Divider(color: AppColor.blackColor),
-                  VerticalSpeacing(20.0),
-                  LabotryNotificationsWidget(),
-                  VerticalSpeacing(10.0),
-                  Divider(color: AppColor.blackColor),
-                  VerticalSpeacing(20.0),
-                  LabotryNotificationsWidget(),
-                  VerticalSpeacing(10.0),
-                  Divider(color: AppColor.blackColor),
-                  VerticalSpeacing(20.0),
-                  LabotryNotificationsWidget(),
-                  VerticalSpeacing(10.0),
-                  Divider(color: AppColor.blackColor),
-                  VerticalSpeacing(20.0),
-                  LabotryNotificationsWidget(),
-                  VerticalSpeacing(10.0),
-                  Divider(color: AppColor.blackColor),
-                  VerticalSpeacing(20.0),
-                  LabotryNotificationsWidget(),
-                  VerticalSpeacing(10.0),
-                  Divider(color: AppColor.blackColor)
-                ],
-              ),
-            ],
+          child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return NotificationsWidget(
+                onpress: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return const PatientDetailDialog();
+                    },
+                  );
+                },
+              );
+            },
           ),
         ),
       ),
